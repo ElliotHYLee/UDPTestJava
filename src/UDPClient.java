db94ec901bb5ebbc8b41b39abb2344d982c02c1c
ElliotHYLee <hylee101001@gmail.com>
2017-07-10 12:57:06 -0400

    sfasdf

--- src/UDPClient.java
+++ src/UDPClient.java
@@ -9,14 +9,16 @@ public class UDPClient {
 	public static void main(String[] args)throws Exception {
 		// TODO Auto-generated method stub
 		BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
-	    DatagramSocket clientSocket = new DatagramSocket();
+		DatagramSocket clientSocket = new DatagramSocket();
 	    InetAddress IPAddress = InetAddress.getByName("localhost");
 	    byte[] sendData = new byte[1024];
 	    byte[] receiveData = new byte[1024];
-	    String sentence = inFromUser.readLine();
+	    String sentence = "Hello, I'm a client";
 	    sendData = sentence.getBytes();
+	    System.out.println("UDP Client: starting");
 	    DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, 9876);
 	    clientSocket.send(sendPacket);
+	    System.out.println("UDP Client: datagram sent");
 	    DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
 	    clientSocket.receive(receivePacket);
 	    String modifiedSentence = new String(receivePacket.getData());
